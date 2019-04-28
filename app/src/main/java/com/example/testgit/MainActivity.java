967af94517f52bb564c9c85434ea9d0d1ef3f25f
Biswajit Karmakar <bkarmak@gmail.com>
2019-04-29 01:29:20 +0600

    HW

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String test = "Hello world";
+        String helloWorld = "Hello world";
+        String helloWorld2 = "Hello world 2";
     }
 }
